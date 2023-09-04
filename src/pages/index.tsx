e88711da413bd33077c4b81cfccc4f1f8fd09f70
Dipesh chauhan <dpshchhn@gmail.com>
2023-09-04 14:22:45 +0545

    logo added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,18 @@ const Home = () => {
         className="h-screen w-full bg-cover bg-no-repeat bg-center"
         style={{ backgroundImage: 'url("/images/homepage4.jpg")' }}
       >
-        <div className="h-full w-[50%] absolute bg-[brown] opacity-25"></div>
+        {/* <div className="h-full w-[50%] absolute bg-[brown] opacity-25"></div> */}
         <div className="flex justify-start items-center">
           {/* <Image src={} alt="logo"/> */}
           <h1 className="text-4xl font-extrabold">
             <span className="text-[brown]">WOOD</span>BERRY INTERIOR AND DECORS
             PRIVATE LIMITED
           </h1>
+        </div>
+        <div>
+          <div className="rounded-full w-40">
+            <img src="/images/Stamp_Woodberry stamp.png" alt="logo" className="w-40 bg-[red]"/>
+          </div>
         </div>
       </div>
     </Layout>
