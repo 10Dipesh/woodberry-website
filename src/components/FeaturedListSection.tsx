30b7c6684cbb1ad105ef7d173493bc82678fcb39
Dipesh chauhan <dpshchhn@gmail.com>
2024-06-16 19:15:06 +0545

    hero section

--- src/components/FeaturedListSection.tsx
+++ src/components/FeaturedListSection.tsx
@@ -1,14 +1,14 @@
 
 const FeaturedListSection = () => {
     return (
-        <section className="mt-4">
+        <section className="mt-4 mx-4">
             <div>
                 <h1>Transform</h1>
                 <h1>Creating Beautiful Spaces</h1>
                 <p>We specialize in residental and commercial interior design.</p>
             </div>
-            <div>
-                <img src="/images/serviceImg.jpg" alt="" />
+            <div className="w-full h-40">
+                <img src="/images/serviceImg.jpg" alt=""  className="w-full h-full h-full object-cover"/>
             </div>
         </section>
     );
